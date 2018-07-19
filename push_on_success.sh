3627824bdbbdbd069223616efe6b80f79f4fba29
Michael De La Rue <mdlr@paddle.com>
2018-07-19 16:18:50 +0100

    set up automatic push to tested in the case of build success

---
+++ push_on_success.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+#
+# Based on https://github.com/Shippable/support/issues/1052
+#
+# * we don't do force pushes ; that ensures that we can't go backwards
+#   or overwrite things if we accidentally (or deliberately to check
+#   something) rerun an old commit.
+#
+
+
+
+if [ "$BRANCH" == "devel" ] && [ "$PULL_REQUEST" == false ]; then
+  git checkout -B tested
+  git push -u origin tested
+fi;
+
+if [ "$BRANCH" == "test_devel" ] && [ "$PULL_REQUEST" == false ]; then
+  git checkout -B tested
+  git push -u origin test_tested
+fi;
+
